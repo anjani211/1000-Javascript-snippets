494c06c772af584347840798ecd8787fd898b5c6
Anjani <jhaanjani57@gmail.com>
2023-07-24 15:54:38 +0530

    2nd Commit

---
+++ stringUpperLower.js
@@ -0,0 +1,6 @@
+let str = "anjani";
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+
+// Explanation: The toUpperCase() method is used to convert all characters in the string to uppercase.
+// Explanation: The toLowerCase() method is used to convert all characters in the string to lowercase.
