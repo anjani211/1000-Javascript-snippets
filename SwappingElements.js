eef0915d550fbaf65dd88e7d791f134e3575765a
Anjani <jhaanjani57@gmail.com>
2023-07-24 13:54:06 +0530

    1st commit

---
+++ SwappingElements.js
@@ -0,0 +1,8 @@
+let a = 5,
+  b = 10;
+[a, b] = [b, a];
+console.log(a, b);
+
+// // This code snippet uses destructuring assignment to swap the values of
+//  variables a and b without using a temporary variable. It creates a temporary
+//  array with the values of a and b, and then assigns the values in reverse order back to the variables.
