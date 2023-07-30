4660a11096c1cab20b3f9c88d004ced4c5cebff4
Anjani <jhaanjani57@gmail.com>
2023-07-30 14:20:13 +0530

    4th commit

---
+++ selectionSort.js
@@ -0,0 +1,14 @@
+function selectionSort(nums) {
+  for (let i = 0; i < nums.length; i++) {
+    let minIndex = i;
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[j] < nums[minIndex]) {
+        minIndex = j;
+      }
+    }
+    [nums[i], nums[minIndex]] = [nums[minIndex], nums[i]];
+  }
+  return nums;
+}
+
+console.log(selectionSort([2, 1, 4, 5, 3]));
