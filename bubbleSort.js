c3d2b16fd3fda9c5e0f6da06eaa25041643d634c
Anjani <jhaanjani57@gmail.com>
2023-07-31 05:53:45 +0530

    6th Commit

---
+++ bubbleSort.js
@@ -0,0 +1,5 @@
+function bubbleSort(nums){
+    for (let i = 0; i < nums.length -1 ;i++) {
+        for(let j=i+1;)
+    }
+}
---
+++ forEach.js
@@ -0,0 +1,5 @@
+let a = [2, 3, 4, 5, 6, 4];
+
+a.forEach(function (val) {
+  console.log("hello, " + val);
+});
